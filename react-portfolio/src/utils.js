f512eee7f2fa616576f5833dd7f2be1c8f7108e0
EliasAlamartimo <115208235+EliasAlamartimo@users.noreply.github.com>
2025-04-10 19:45:06 +0300

    empty project

---
+++ react-portfolio/src/utils.js
@@ -0,0 +1,9 @@
+const images = import.meta.glob('../assets/**/*', { eager: true });
+
+
+export const getImageUrl = (path) => {
+    const imageRelativePath = path;
+    // Dynamically resolve the image import
+    const imagePath = images[`../assets/${imageRelativePath}`].default;
+    return imagePath;
+};
